51e9e939eac0e139f280066d702e8c29593b1a40
Aleksandr <bestprogreraxe@gmail.com>
2023-03-31 04:15:33 +0300

    Solved Task12

--- Task12/main.py
+++ Task12/main.py
@@ -4,3 +4,15 @@
 Он называет сумму этих чисел S и их произведение P. Помогите Кате отгадать задуманные Петей числа.
 """
 
+prod = int(input('Enter product of numbers (P): '))
+sum = int(input('Enter sum of numbers (S): '))
+first_number = 0
+second_number = 0
+while first_number * second_number != prod or first_number + second_number != sum:
+    first_number += 1
+    second_number = prod / first_number
+    if first_number == prod and first_number > 1:
+        print('Wrong product/sum pair.')
+        break
+    elif first_number * second_number == prod and first_number + second_number == sum:
+        print(f'X = {first_number}, Y = {int(second_number)} ')
