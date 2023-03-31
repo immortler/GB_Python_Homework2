e76e2811e641c4e28d12d39038b13ef11838cab3
Aleksandr <bestprogreraxe@gmail.com>
2023-03-31 04:05:04 +0300

    Solved Task10

--- Task10/main.py
+++ Task10/main.py
@@ -4,3 +4,11 @@
 минимальное количество монет, которые нужно перевернуть
 ###
 
+N = int(input('Enter a number of coins:'))
+obv = 0
+for i in range (0, N):
+    side = int(input('Enter 1 for obverse of coin and 0 for back side:'))
+    if side==1:
+        obv += 1
+print('Minimal flips for getting the same sides:')
+print(obv if N/2>obv else N-obv)
