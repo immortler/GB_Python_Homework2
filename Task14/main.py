e06971bcf4a0f2028298553af9f5516751a8c4fd
Aleksandr <bestprogreraxe@gmail.com>
2023-03-31 04:27:08 +0300

    Solved Task14

--- Task14/main.py
+++ Task14/main.py
@@ -1,4 +1,14 @@
 # Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
 
-
-
+n = int(input('Enter a number above 1 for check: '))
+pow_of_2 = 2
+if n >= pow_of_2:
+    i = 1
+    print(f'Powers of 2 that not above {n}:')
+    while pow_of_2 <= n:
+        print(f'2^{i} = {pow_of_2}')
+        pow_of_2 = pow_of_2 * 2
+        i += 1
+    print('Thanks for using products of Tardy Inc.')
+else:
+    print(f'{n} is less than 2, sorry. ')
